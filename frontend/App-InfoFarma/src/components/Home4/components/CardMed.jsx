4bbc1db553d63f925dfc63c3b1da672d89df394d
JonathanDiz <jdiazolivares007@gmail.com>
2023-10-11 22:12:16 -0300

    front actualizado y sevidor scraper update

--- frontend/App-InfoFarma/src/components/Home4/components/CardMed.jsx
+++ frontend/App-InfoFarma/src/components/Home4/components/CardMed.jsx
@@ -2,8 +2,13 @@ import { Box, Button } from '@mui/material'
 import './CardMed.css'
 
 export const CardMed = ({ product }) => {
+    if (!product) {
+        // Manejo de caso en el que product es undefined
+        return <div>No hay datos de productos</div>;
+    }
+    
     const { medicamento, imagen } = product
-
+    
     return (
         <Box className='box-card'>
             <img className='img-card'
