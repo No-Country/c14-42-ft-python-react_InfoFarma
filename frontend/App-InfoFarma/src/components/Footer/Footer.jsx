89f3c503a139491a3e84152f9c01a891c8f00a4a
Jareth Guerrero <yaredguerrero@gmail.com>
2023-10-15 19:26:47 -0600

    -Creación customHook useEmail. -Footer Finalizado, esperando revisión y aprobación del equipo.

--- frontend/App-InfoFarma/src/components/Footer/Footer.jsx
+++ frontend/App-InfoFarma/src/components/Footer/Footer.jsx
@@ -1,45 +1,70 @@
 import React from 'react';
 import { Container, Paper, Button, TextField, Grid, Typography, Link, } from '@mui/material';
-import { IoLogoGithub, IoLogoFacebook, IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io5';
+import { IoLogoGithub, IoLogoSlack, IoLogoLinkedin } from 'react-icons/io5';
 import { IconContext } from 'react-icons';
+import  useEmail  from '../../customHooks/useEmail'
 import './Footer.css';
 
 
 export const Footer = () => {
 
+  const {
+    email,
+    emailError,
+    handleEmailCheck,
+    handleSubscribe
+  } = useEmail()
+
+  const handleIconClick = (link) => {
+    window.location.href = link
+  }
+
   return (
     <Container className="footer-container">
       <Paper elevation={3} square={false} className="footer-paper">
         <Grid container spacing={4}>
-          {/* Social Media Section */}
           <Grid item xs={12} md={6}>
             <Typography variant="h6" component="div" gutterBottom>
               Nuestras redes sociales
             </Typography>
             <IconContext.Provider value={{ size: '2em' }}>
               <div className="social-media-icons">
-                <IoLogoGithub />
-                <IoLogoFacebook />
-                <IoLogoInstagram />
-                <IoLogoLinkedin />
+                <Link href='https://github.com/No-Country' color={'inherit'}
+                  target='_blank' rel="noopener noreferrer"
+                  onClick={() => handleIconClick('https://github.com/No-Country')}>
+                  <IoLogoGithub />
+                </Link>
+                <Link href='https://join.slack.com/t/no-country/shared_invite/zt-23wenaufp-caQgYqwKAsc6Qdjh6uYAbQ' color={'inherit'}
+                  target='_blank' rel="noopener noreferrer"
+                  onClick={() => handleIconClick('https://join.slack.com/t/no-country/shared_invite/zt-23wenaufp-caQgYqwKAsc6Qdjh6uYAbQ')}>
+                  <IoLogoSlack />
+                </Link>
+                <Link href='https://www.linkedin.com/company/nocountrytalent/' color={'inherit'}
+                  target='_blank' rel="noopener noreferrer"
+                  onClick={() => handleIconClick('https://www.linkedin.com/company/nocountrytalent/')}>
+                  <IoLogoLinkedin />
+                </Link>
               </div>
             </IconContext.Provider>
           </Grid>
 
-          {/* Form Section */}
           <Grid item xs={12} md={6}>
             <Typography variant="h6" component="div" gutterBottom>
               Regístrate para recibir noticias y promociones
             </Typography>
-            <form>
+            <form onSubmit={handleSubscribe}>
               <Grid container spacing={2} alignItems="center">
                 <Grid item xs={12} md={6}>
                   <TextField
                     id="email"
-                    label="Email address"
+                    label="Correo electrónico"
                     variant="outlined"
                     fullWidth
                     className="form-input"
+                    value={email}
+                    onChange={handleEmailCheck}
+                    error={Boolean(emailError)}
+                    helperText={emailError}
                   />
                 </Grid>
                 <Grid item xs={12} md={6}>
@@ -59,10 +84,9 @@ export const Footer = () => {
 
         {/* Text Section */}
         <Typography variant="body1" paragraph>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-          distinctio earum repellat quaerat voluptatibus placeat nam, commodi
-          optio pariatur est quia magnam eum harum corrupti dicta, aliquam sequi
-          voluptate quas.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Rem dolores quia, expedita quibusdam necessitatibus quaerat distinctio neque voluptates quasi,
+          officiis facere id tenetur beatae error sapiente itaque consequuntur dolor facilis?
         </Typography>
 
         {/* Links Section */}
@@ -70,7 +94,7 @@ export const Footer = () => {
           {[...Array(2)].map((_, index) => (
             <div key={index}>
               <Typography variant="h6" component="div" gutterBottom>
-              Colaboradores  
+                Colaboradores
               </Typography>
               <ul className="list">
                 {[...Array(5)].map((_, subIndex) => (
@@ -98,7 +122,7 @@ export const Footer = () => {
           nocountry.tech
         </Link>
       </Typography>
-    </Container>
+    </Container >
   );
 };
 
