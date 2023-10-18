aeccc48702d9778a83c94396c5ed99bb52cb91f5
Melina Fuch <sosafuchmelina@gmail.com>
2023-10-18 11:20:50 -0300

    add and update css components

--- frontend/App-InfoFarma/src/components/Home/IconsInfo/Icons&Info.jsx
+++ frontend/App-InfoFarma/src/components/Home/IconsInfo/Icons&Info.jsx
@@ -5,81 +5,51 @@ import styled from '@emotion/styled';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-const PageContainer = styled(Container)`
-  padding-top: 5.5em;
-  padding-bottom: 5em;
-  background-color: #E2FCD4;
-  min-height: 100vh;
-  min-width: 100vw;
-  margin: 0 !important;
-  font-family: 'Roboto Condensed';
-  color: #2F3D2D;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Title = styled('div')`
-    font-size: 1em;
-    text-align: center;
-    margin: 1rem 0;
-    margin-bottom: 1rem;
-
-    @media (min-width: 600px) {
-        font-size: 1.2rem;
-    }
-  
-    @media (min-width: 960px) {
-        font-size: 1.4rem;
-    }
-`;
-
-const Text = styled('div')`
-  font-size: 1em;
-  text-align: center;
-  width: 10em;
-  
-  @media (min-width: 600px) {
-      font-size: 1.1em;
-    }
-
-  @media (min-width: 900px) {
-    font-size: 1.4em;
-  }
-`;
+const Img = styled("img")({
+  width: 130,
+  height: "100%",
+  objectFit: "contain",
+});
 
 export default function IconsInfo() {
   return (
-    <PageContainer>
-      <Title>
-        <h1>En InfoFarma vas a encontrar:</h1>
-      </Title>
-    {/* <h1></h1> */}
-      <Grid container spacing={2}>
+    <Container
+    maxWidth="100vw"
+    sx={{
+        bgcolor: '#E2FCD4',
+        p: 5,
+        textAlign: 'center',
+        placeItems: 'center'
+      }}>
+        <Typography 
+        variant='h4' 
+        component='h2'
+        p={5}
+        pt={7}
+        >
+          En InfoFarma vas a encontrar:</Typography >
+      <Grid container spacing={5} pb={10} >
         {itemData.map((item) => (
-          <Grid item key={item.img} xs={6} md={4}>
-            <Paper
-              css={css`
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                padding: 3em;
-                box-shadow: none;
-                background-color: #E2FCD4;
-              `}
-            >
-              <img
+          <Grid 
+            item 
+            key={item.img} 
+            xs={12} 
+            sm={6}
+            md={4} 
+            pb={7}
+          >
+              <Img
+                pb={5}
+                justifyContent='center'
                 src={item.img}
                 alt={item.description}
-                width={130}
               />
-              <Text>
-                <h4>{item.description}</h4>
-              </Text>
-            </Paper>
+              <Typography 
+              justifyContent='center' variant='h5' component='h4'>{item.description}</Typography>
           </Grid>
         ))}
       </Grid>
-    </PageContainer>
+    </Container>
   );
 }
 
