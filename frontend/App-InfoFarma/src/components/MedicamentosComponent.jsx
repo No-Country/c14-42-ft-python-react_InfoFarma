a10342b7839cd81d5298032c08cf7f112b9b65b9
Melina Fuch <sosafuchmelina@gmail.com>
2023-10-18 21:55:43 -0300

    add route getProducts in redux and your config

--- frontend/App-InfoFarma/src/components/MedicamentosComponent.jsx
+++ frontend/App-InfoFarma/src/components/MedicamentosComponent.jsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllProducts } from '../redux/actions';
 
 function MedicamentosComponent() {
   const [data, setData] = useState([]);
+  const dispatch = useDispatch()
+
+  const allProducts = useSelector(state => state.allProducts);
 
   useEffect(() => {
     // Realiza una solicitud GET al backend de FastAPI
-    fetch('http://localhost:3000/medicamentos/enfermedades%20infecciosas%20y%20parasitarias')
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+    // fetch('http://localhost:3000/medicamentos/enfermedades%20infecciosas%20y%20parasitarias')
+    //   .then((response) => response.json())
+    //   .then((data) => setData(data));
+    dispatch(getAllProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Medicamentos</h1>
+      {/* {console.log({allProducts})} */}
       <ul>
-        {data.map((medicamento) => (
-          <li key={medicamento.medicamento}>{medicamento.medicamento}</li>
+        {allProducts.map((medicamento) => (
+          <li key={medicamento.id}>{medicamento.name}</li>
         ))}
       </ul>
     </div>
