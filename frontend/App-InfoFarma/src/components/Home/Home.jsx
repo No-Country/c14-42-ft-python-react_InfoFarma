e9a932b1c3da89e0790b0096330a1c9ae407c840
Jareth Guerrero <yaredguerrero@gmail.com>
2023-10-31 11:49:07 -0600

    Trabajando en el mapa

--- frontend/App-InfoFarma/src/components/Home/Home.jsx
+++ frontend/App-InfoFarma/src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import { Footer } from "../Footer/Footer"
+import { Map } from "../Map/Map"
 import { HeroSection } from "./HeroSection/HeroSection"
 import './Home.css'
 import IconsInfo from "./IconsInfo/Icons&Info"
 import { Info } from "./Info/Info"
+
+
 function Home() {
   return (
     <>
       <HeroSection />
+      <Map />
       <IconsInfo />
       <Info />
       <Footer />
--- frontend/App-InfoFarma/src/components/Map/Map.jsx
+++ frontend/App-InfoFarma/src/components/Map/Map.jsx
@@ -1,71 +1,99 @@
 import React, { useEffect, useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { Loader } from '@googlemaps/js-api-loader';
+import { Button } from '@mui/material'
 
-const Map = () => {
-  // Configura las coordenadas del centro del mapa (por defecto, inicia en una ubicación)
-  const defaultCenter = { lat: 19.4326068, lng: -99.1357798 };
+export const Map = () => {
+  const apiKey = 'AIzaSyCwV3RBVfWLMFRGmX-I-wa7x5xH1rwOCXM'; // Reemplaza con tu clave de API de Google Maps
+  const searchRadius = 2000; // Radio de búsqueda en metros
 
-  // Configura el radio de búsqueda (en metros)
-  const searchRadius = 1500;
+  const [selectedPharmacy, setSelectedPharmacy] = useState(null);
 
-  // Clave de API de Google
-  const apiKey = 'AIzaSyBedIuI_wGuP6pbEysub4bash7L3Aa0zds';
-
-  // Estados para almacenar los marcadores de las farmacias
-  const [pharmacies, setPharmacies] = useState([]);
-
-  // Función para obtener farmacias cercanas
-  const getNearbyPharmacies = (position) => {
-    const { lat, lng } = position;
-
-    // Realiza una solicitud a la API de Places
-    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${searchRadius}&type=pharmacy&key=${apiKey}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Actualiza el estado con los datos de las farmacias cercanas
-        setPharmacies(data.results);
-      })
-      .catch((error) => {
-        console.error('Error al obtener farmacias cercanas:', error);
-      });
-  };
-
-  // Obtén la ubicación del usuario cuando se monta el componente
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        // Obtiene la ubicación del usuario y llama a la función para obtener farmacias cercanas
-        const userPosition = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        getNearbyPharmacies(userPosition);
+    const loader = new Loader({
+      apiKey: apiKey,
+      version: 'weekly',
+      libraries: ['places'],
+    });
+
+    loader.load().then(google => {
+      // Crear un mapa
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 0, lng: 0 },
+        zoom: 15, // Ajusta el nivel de zoom según tus preferencias
       });
-    }
+
+      // Obtener la ubicación del usuario
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          const userLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          // Centrar el mapa en la ubicación del usuario
+          map.setCenter(userLocation);
+
+          // Realizar una búsqueda de farmacias cercanas
+          const placesService = new google.maps.places.PlacesService(map);
+          placesService.nearbySearch({
+            location: userLocation,
+            radius: searchRadius,
+            type: 'pharmacy',
+          }, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+              // Personalizar marcadores para cada farmacia encontrada
+              results.forEach(pharmacy => {
+                const marker = new google.maps.Marker({
+                  position: pharmacy.geometry.location,
+                  map: map,
+                  title: pharmacy.name,
+                });
+
+                // Agregar un evento de clic al marcador
+                marker.addListener('click', () => {
+                  setSelectedPharmacy(pharmacy);
+                });
+              });
+            } else {
+              console.error('Error al buscar farmacias:', status);
+            }
+          });
+
+          // Agregar un marcador en la ubicación del usuario
+          new google.maps.Marker({
+            position: userLocation,
+            map: map,
+            title: "Tu ubicación",
+          });
+        }, error => {
+          console.error('Error al obtener la ubicación:', error);
+        });
+      } else {
+        console.error('El navegador no admite geolocalización.');
+      }
+    });
   }, []);
 
+  // Función para abrir la ubicación en Google Maps
+  const openInGoogleMaps = (location) => {
+    const lat = location.lat();
+    const lng = location.lng();
+    const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+    window.open(url, '_blank');
+  };
+
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap
-        mapContainerStyle={{ width: '100%', height: '400px' }}
-        center={defaultCenter}
-        zoom={8}
-      >
-        {/* Renderiza marcadores para las farmacias */}
-        {pharmacies.map((pharmacy) => (
-          <Marker
-            key={pharmacy.place_id}
-            position={{
-              lat: pharmacy.geometry.location.lat,
-              lng: pharmacy.geometry.location.lng,
-            }}
-            // Puedes personalizar los marcadores aquí
-            // icon={{ url: 'ruta/de/imagen.png', scaledSize: { width: 30, height: 30 } }}
-          />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <div>
+      <div id="map" style={{ width: '100%', height: '400px' }}></div>
+      {selectedPharmacy && (
+        <div>
+          <h2>Farmacia Seleccionada</h2>
+          <p>Nombre: {selectedPharmacy.name}</p>
+          <p>Dirección: {selectedPharmacy.vicinity}</p>
+          <Button onClick={() => openInGoogleMaps(selectedPharmacy.geometry.location)} variant="contained" color="primary">Ir a farmacia</Button>
+          {/* Agrega más detalles de la farmacia según tus necesidades */}
+        </div>
+      )}
+    </div>
   );
 };
-
-export default Map;
