c2cfab9f016feea065f139c117003f1a1662d133
Melina Fuch <sosafuchmelina@gmail.com>
2023-11-01 15:46:07 -0300

    refactorized styles mt

--- frontend/App-InfoFarma/src/components/PageNewProd/PageNewProd.jsx
+++ frontend/App-InfoFarma/src/components/PageNewProd/PageNewProd.jsx
@@ -80,10 +80,12 @@ export default function PageNewProd () {
         display: 'flex',
         flexDirection: 'column',
         maxWidth: '32rem',
-        margin: '3rem auto',
-        padding: '0 2rem'
+        m: '0 auto',
+        mt: '8rem',
+        mb: '6rem',
+        p: '0 2rem'
       }}>
-        <Typography variant="h5" sx={{ marginBottom: '1rem', textAlign: 'center' }}>Agregar un nuevo producto</Typography>
+        <Typography variant="h4" color={'primary'} sx={{ mb: '1rem', textAlign: 'center' }}>Agregar un nuevo producto</Typography>
         <Formik
           initialValues={initialValues}
           validate={validate}
